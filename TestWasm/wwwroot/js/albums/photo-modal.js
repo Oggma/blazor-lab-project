1753b2292fb3ad6eced9495918d7a4ed55a25b2f
Roman Svezhentsev <raman.svezhantsau@gmail.com>
2021-02-28 13:18:04 +0300

    refactor photo comp + photo modal

---
+++ TestWasm/wwwroot/js/albums/photo-modal.js
@@ -0,0 +1,11 @@
+﻿export function open(modalId) {
+    var modal = document.getElementById(modalId);
+    console.log(modal);
+    modal.style.display = "block";
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
