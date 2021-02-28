7ade3ef3cf8d73c3c142f90da1cf5cfee6aa05be
Raman Svezhantsau <raman.svezhantsau@gmail.com>
2021-02-28 15:30:31 +0300

    Photos page
    * fix: thumbnail flex
    
    * refactor photo comp + photo modal
    
    * fix modal
    
    * fix modal content
    
    * photo modal as component
    
    * modal close btn

---
+++ TestWasm/wwwroot/js/albums/photo-modal.js
@@ -0,0 +1,15 @@
+﻿export function open(modalId) {
+    let modal = document.getElementById(modalId);
+    modal.style.display = "block";
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            close(modalId);
+        }
+    }
+}
+
+export function close(modalId) {
+    let modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
